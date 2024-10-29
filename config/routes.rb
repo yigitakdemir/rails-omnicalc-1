9d43e4a36f5cb776ec23aaba050f314b71753e65
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-10-29 04:08:37 +0000

    5m 2308

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "omni", :action => "square"})
+
+  get("/square/new", { :controller => "omni", :action => "square_new"})
+
+  get("/square/results", { :controller => "omni", :action => "square_results"})
+
+  get("/square_root/new", { :controller => "omni", :action => "square_root_new"})
+
+  get("/square_root/results", { :controller => "omni", :action => "square_root_results"})
+
+  get("/random/new", { :controller => "omni", :action => "random_new"})
+
+  get("/random/results", { :controller => "omni", :action => "random_results"})
+
+  get("/payment/new", { :controller => "omni", :action => "payment_new"})
+
+  get("/payment/results", { :controller => "omni", :action => "payment_results"})
 end
